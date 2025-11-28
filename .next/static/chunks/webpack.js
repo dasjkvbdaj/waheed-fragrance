4f3308bc1b16027d04ad4dba516459ef9271bd37
Ali <afakhreddine717@gmail.com>
2025-11-28 13:01:16 +0200

    -changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "966fe5b93e328cc1"; }
+/******/ 		__webpack_require__.h = function() { return "67a97c74baa0f1b4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
