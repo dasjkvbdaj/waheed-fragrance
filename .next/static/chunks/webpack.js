470608914c54853d0f84f37a534cf1e2c67ed073
Ali <afakhreddine717@gmail.com>
2025-12-06 22:49:02 +0200

    updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4452d885c0835541"; }
+/******/ 		__webpack_require__.h = function() { return "cd0e412bd8f16e9e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
