46f6575a0d7ece4a93c3af33b133b55059564228
Ali <afakhreddine717@gmail.com>
2025-12-07 12:20:44 +0200

    updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cd0e412bd8f16e9e"; }
+/******/ 		__webpack_require__.h = function() { return "653dd4f3eac69976"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.b46ca75e26cb8ddd.hot-update.js
+++ .next/static/webpack/webpack.b46ca75e26cb8ddd.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "846c320e05a6f908"; }
+/******/ 	__webpack_require__.h = function() { return "653dd4f3eac69976"; }
 /******/ }();
 /******/ 
 /******/ }
